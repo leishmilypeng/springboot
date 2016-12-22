e82b14c1e5cd3bd20e38f0405d5deadff7028400
leishmilypeng <891880796@qq.com>
2016-12-22 17:29:09 +0800

    泛型类

--- src/main/java/com/lp/dao/IBaseDao.java
+++ src/main/java/com/lp/dao/IBaseDao.java
@@ -1,11 +1,13 @@
 package com.lp.dao;
 
 
+import org.springframework.data.repository.CrudRepository;
+
 import java.io.Serializable;
 
 /**
  * Created by CPR161 on 2016-12-22.
  */
-public interface IBaseDao<T,ID extends Serializable>  {
-    void save(T t);
+public interface IBaseDao<T,ID extends Serializable>   {
+    void test(T t);
 }
